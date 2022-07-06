a030b4d5a65fb0970bfe1ab5de2dcc4d8c8faaed
Paulo Sheliga <paulosheliga@gmail.com>
2022-07-06 14:12:10 -0300

    feat: projeto funcional

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import os
 from flask import Flask, render_template, request, session,redirect, url_for, send_file
+from numpy import save
 from werkzeug.utils import secure_filename
+import PyPDF2
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 UPLOAD_FOLDER = os.path.join(os.getcwd(),'upload') 
+
+def open_file(file):
+    if file:
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+        return page_content
 
 @app.route('/')
 def index():
@@ -13,8 +22,11 @@ def index():
 def upload():
     file = request.files['pdf']
     savePath = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
-    file.save(savePath)
-    return render_template('index.html',text = "teste")
+    file.save(savePath)    
+    print(savePath)
+    
+    texto = open_file(savePath)
+    return render_template('index.html',text = texto)
     #return 'Upload com sucesso'
 
 @app.route('/get-file/<filename>')
